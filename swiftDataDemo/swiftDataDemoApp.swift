27ae3937d60796e553d55528882187014e584a00
muhammed <mohammadalaaraji@gmail.com>
2024-08-19 01:36:25 +0300

    Initial Commit

---
+++ swiftDataDemo/swiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftDataDemoApp.swift
+//  swiftDataDemo
+//
+//  Created by Muhammad Alaraji on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
